25e9b87245b70b299de688446cf27760643ebb1e
ProfeNico <91351721+ProfeNico@users.noreply.github.com>
2021-10-01 21:27:19 -0300

    Add files via upload

---
+++ contadores.py
@@ -0,0 +1,31 @@
+varNumero1 = 0
+varIncrementador = 1
+
+varNumero1 = varNumero1 + varIncrementador
+
+print(varNumero1)
+
+print("el piepe puro que desordena")
+
+varNumero1 = varNumero1 + varIncrementador
+
+print(varNumero1)
+
+print("don mandell puro frio")
+
+varNumero1 = varNumero1 + varIncrementador
+
+print(varNumero1)
+
+print("--------------------------------------")
+
+varRango1 = 0
+varRango2 = 3
+
+for varCont in range(varRango1,varRango2):
+    print("la secuencia es: "+str(varCont))
+
+varArreglo = ["a","e","i","o","u"]
+
+for varCont1 in varArreglo:
+    print("las vocales son: "+varCont1)
